813baefa49426628d64596960148e63149eb660a
momijiame <amedama.ginmokusei@gmail.com>
2014-03-20 23:21:21 +0900

    Update

--- centos/scripts/vboxkmod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-  mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-  sh /mnt/VBoxLinuxAdditions.run
-  umount /mnt
-  rm -rf /home/vagrant/VBoxGuestAdditions.iso
-fi
-
